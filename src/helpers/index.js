51b9667c85e92d4e74de361bb603366d5e5e19bd
Benedito Carlos <beneditocarlos.bc@gmail.com>
2022-11-18 14:45:32 -0300

    maps static google

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -154,4 +154,34 @@ export default {
 
        return exames.length ? this.sum(exames) : 0
     },
+
+    // GMapCircle(lat,lng,rad,detail=8){
+    //     var uri = 'https://maps.googleapis.com/maps/api/staticmap?key=' + API_KEY;
+    //     // https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
+    //     // &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
+    //     // &markers=color:red%7Clabel:C%7C40.718217,-73.998284
+    //     // &key=YOUR_API_KEY&signature=YOUR_SIGNATURE
+
+    //     // var staticMapSrc = '&center=' + lat + ',' + lng;
+    //     var staticMapSrc = '&center=Brooklyn+Bridge,New+York,NY'
+    //     staticMapSrc += '&size=850x650&zoom=13';
+    //     staticMapSrc += '&path=color:0xff0000ff:weight:1';
+
+    //     var r    = 6371;
+    //     var pi   = Math.PI;
+    //     var _lat  = (lat * pi) / 180;
+    //     var _lng  = (lng * pi) / 180;
+    //     var d    = (rad/1000) / r;
+    //     var i = 0;
+
+    //     for(i = 0; i <= 360; i+=detail) {
+    //         var brng = i * pi / 180;
+    //         var pLat = Math.asin(Math.sin(_lat) * Math.cos(d) + Math.cos(_lat) * Math.sin(d) * Math.cos(brng));
+    //         var pLng = ((_lng + Math.atan2(Math.sin(brng) * Math.sin(d) * Math.cos(_lat), Math.cos(d) - Math.sin(_lat) * Math.sin(pLat))) * 180) / pi;
+    //         pLat = (pLat * 180) / pi;
+    //        staticMapSrc += "|" + pLat + "," + pLng;
+    //     }
+
+    //     return uri + encodeURI(staticMapSrc);
+    // }
 }
