9fb99ddafd7330b5c8a4bc25cf7d8f18a4f7ec65
Benedito Carlos <beneditocarlos.bc@gmail.com>
2022-12-01 22:20:05 -0300

    loading animation tweaks

--- src/components/scorecharts/Spinner.vue
+++ src/components/scorecharts/Spinner.vue
@@ -1,6 +1,24 @@
 <template>
-  <div class="d-flex align-items-center m-2">
-    <strong>Loading...</strong>
-    <div class="spinner-border ms-auto" role="status" aria-hidden="true"></div>
-  </div>
+  <main>
+    <div class="container mt-5">
+      <div class="row justify-content-md-center text-center">
+        <div class="col-sm-12 col-md-6 col-lg-4 mt-5">
+          <img
+            src="@/assets/aquila.png"
+            class="rounded mx-auto d-block animate__animated animate__bounce animate__infinite"
+            width="200"
+          />
+
+          <div class="d-flex justify-content-center">
+            <strong>
+              <div class="spinner-border spinner-border-sm" role="status">
+                <span class="visually-hidden">Loading...</span>
+              </div>
+              Loading...
+            </strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
--- src/views/scorecharts/HomeView.vue
+++ src/views/scorecharts/HomeView.vue
@@ -1,22 +1,15 @@
 <template>
   <main>
-    <div class="container-fluid mt-5">
-      <div class="row justify-content-md-center">
-        <div class="col mt-5">
+    <div class="container mt-5">
+      <div class="row justify-content-md-center text-center">
+        <div class="col-sm-12 col-md-6 col-lg-4 mt-5">
           <img
             src="@/assets/aquila.png"
             class="rounded mx-auto d-block animate__animated animate__bounce"
-            width="200"
+            width="250"
           />
         </div>
       </div>
     </div>
   </main>
 </template>
-
-<script>
-</script>
-
-<style>
-  body { background-color: #fff; }
-</style>
